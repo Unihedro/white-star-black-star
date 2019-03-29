8ee3c42bfab92cffacd7dac4ac73bb7942f6e868
Glitch (hello-webpage) <none>
2019-03-29 20:25:33 +0000

    🌖🍯 Checkpoint ./script.js:1035398/1406

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-var things = localStorage.things ? JSON.parse(localStorage.things) : [0,0,0]
+var things = localStorage.things ? JSON.parse(localStorage.things) : [0,0,0,[],[],[],[],[],[]]
 
-var resKeys = ['red','green','blue']
+const resKeys = ['red','green','blue']
+const amountOfThings = resKeys.length
 var focus = null;
 var selectedPanel = 'red'
 var changePanel = null
 var changeProgress = 0
-var tasksLeft = {red: [
+const tasks = {red: [
   {name:'■',cost:[8,0,0]},
   {name:'□',cost:[5,5,5]},
   {name:'▤',cost:[24,0,0],moreCost:[{type:'□',amount:1,any:1}]},
-  {name:'▥',cost:[16,16,16],moreCost:[{type:'□',amount:1,any:1}]},
+  {name:'▥',cost:[16,16,16],moreCost:[{type:'□',amount:1,any:1}]}, // storage + 
   {name:'▦',cost:[8,16,16],moreCost:[{type:'▤',amount:1},{type:'▥',amount:1}]},
-   ,'','','','','▣'], green:[],blue:[]}
+  {name:'▣',cost:[],moreCost:[{type:'□',amount:1},{type:'□',amount:5,any:1}]}
+], green:[],blue:[]}
+var tasksLeft = {red:[],green:[],blue:[]}
 function render() {
   [red.innerText, green.innerText, blue.innerText] = things;
   panelSwitcher.innerText = changePanel ? ['','☆★','☆☆','★☆','★★'][changeProgress] : ""
@@ -20,9 +23,16 @@ function render() {
     tasks.innerHTML = ""
   }
 }
+function getResourceCap(thing){
+  var initial = 50
+  return initial;
+}
 
 function gameTick(){
-  focus && things[resKeys.indexOf(focus)]++
+  if (focus) {
+    const thing = resKeys.indexOf(focus);
+    things[thing] < getResourceCap(thing) && things[thing]++
+  }
   if (changePanel && selectedPanel != changePanel) {
       if (++changeProgress == 5)
         [selectedPanel, changePanel] = [changePanel, null]
